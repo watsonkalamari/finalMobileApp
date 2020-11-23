174a25ad65fba2f6fcea08aac18161ef44fb29d4
lgnowlin <lgnowlin@ualr.edu>
2020-11-23 11:38:13 -0600

    Sign Up and Sign In Complete

---
+++ app/src/main/java/com/example/whatsfordinner/SignInActivity.java
@@ -0,0 +1,13 @@
+package com.example.whatsfordinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in_activity);
+    }
+}
---
+++ app/src/main/java/com/example/whatsfordinner/SignUpActivity.java
@@ -0,0 +1,13 @@
+package com.example.whatsfordinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up_activity);
+    }
+}
