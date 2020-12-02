38e7fed4292964371d252d55bee09570d0124c70
lgnowlin <lgnowlin@ualr.edu>
2020-12-02 17:03:13 -0600

    Start on Database

---
+++ app/src/main/java/com/example/whatsfordinner/Adapter/UserAdapter.java
@@ -0,0 +1,101 @@
+package com.example.whatsfordinner.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.ualr.roomdatabase.R;
+import edu.ualr.roomdatabase.db.entity.User;
+import edu.ualr.roomdatabase.ui.OnUserSelectedListener;
+
+public class UserAdapter extends RecyclerView.Adapter {
+
+    private List<User> mItems = new ArrayList<>();
+    private OnUserSelectedListener userSelectionListener;
+
+    public void updateItems(List<User> users) {
+        this.mItems = users;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_list_item, parent, false);
+        return new UserViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        User user = mItems.get(position);
+        UserViewHolder vh = (UserViewHolder) holder;
+        vh.firstNameTV.setText(user.getName());
+        vh.lastNameTV.setText(user.getLastName());
+        vh.streetTV.setText(user.getAddress().getStreet());
+        vh.stateTV.setText(user.getAddress().getState());
+        vh.cityTV.setText(user.getAddress().getCity());
+        vh.postcodeTV.setText(String.valueOf(user.getAddress().getPostCode()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.mItems.size();
+    }
+
+    public void setUserDeletionListener(OnUserDeletedListener listener) {
+        this.userDeletionListener = listener;
+    }
+
+    public void setUserSelectionListener(OnUserSelectedListener listener) {
+        this.userSelectionListener = listener;
+    }
+
+    public interface OnUserDeletedListener {
+        void onUserDeleted(User user);
+    }
+
+    public class UserViewHolder extends RecyclerView.ViewHolder {
+
+        public ConstraintLayout containerLyt;
+        public TextView firstNameTV;
+        public TextView lastNameTV;
+        public TextView streetTV;
+        public TextView stateTV;
+        public TextView cityTV;
+        public TextView postcodeTV;
+        public ImageButton deleteBtn;
+
+        public UserViewHolder(View v) {
+            super(v);
+            containerLyt = v.findViewById(R.id.container);
+            containerLyt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    userSelectionListener.onUserSelected(mItems.get(getAdapterPosition()), getAdapterPosition());
+                }
+            });
+            firstNameTV = v.findViewById(R.id.first_name_tv);
+            lastNameTV = v.findViewById(R.id.last_name_tv);
+            streetTV = v.findViewById(R.id.street_tv);
+            stateTV = v.findViewById(R.id.state_tv);
+            cityTV = v.findViewById(R.id.city_tv);
+            postcodeTV = v.findViewById(R.id.postcode_tv);
+            deleteBtn = v.findViewById(R.id.user_delete_btn);
+            deleteBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    userDeletionListener.onUserDeleted(mItems.get(getAdapterPosition()));
+                }
+            });
+        }
+    }
+}
---
+++ app/src/main/java/com/example/whatsfordinner/ViewHolder/UserViewHolder.java
@@ -0,0 +1,85 @@
+package com.example.whatsfordinner.ViewHolder;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import edu.ualr.roomdatabase.db.AppDatabase;
+import edu.ualr.roomdatabase.db.entity.User;
+
+public class UserViewModel extends AndroidViewModel {
+    // TODO 28. Define a member to store a reference to the database
+    private AppDatabase database;
+    // TODO 29. Define a LiveData member used as observable field to automatically update the UI when
+    // values returned from data queries are received
+    private LiveData<List<User>> users;
+
+    // TODO 30. Instead of retrieving the list of users from the database, retrieve the LiveData
+    //  object from the DAO and use it to subscribe to changes
+
+    // TODO 31. Define the constructor of the viewmodel
+    public UserViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    // TODO 32. Define a method to setup the database
+    public void setupDB() {
+        // TODO 32.01. Get a reference to the database
+        database = AppDatabase.getInstance(getApplication());
+        // TODO 32.02. Subscribe to db changes
+        users = database.getUserDao().loadAllUsers();
+    }
+
+    // TODO 33. Populate the database with data. We'll reset the database to start from scratch every time we launch the app
+    public void populateAppDatabase(final List<User> dataset) {
+        new AsyncTask<List<User>, Void, Void>(){
+            @Override
+            protected Void doInBackground(List<User>... userLists) {
+                database.getUserDao().deleteAll();
+                database.getUserDao().insertUsers(userLists[0].toArray(new User[userLists[0].size()]));
+                return null;
+            }
+        }.execute(dataset);
+    }
+
+    // TODO 34. Define a get method to allow the activity subscribe to changes in database
+    public LiveData<List<User>> getUsers() {
+        return this.users;
+    }
+
+    // TODO 36. Define a method to delete a user from the database
+    public void deleteUser(User user) {
+        new AsyncTask<User, Void, Void>(){
+            @Override
+            protected Void doInBackground(User... users) {
+                database.getUserDao().deleteUser(users[0]);
+                return null;
+            }
+        }.execute(user);
+    }
+
+    // TODO 38. Define a method to edit and update an user in the database
+    public void updateUser(String name, String lastName, String street, String state, String city, int postCode, int index) {
+        User user = users.getValue().get(index);
+        user.setName(name);
+        user.setLastName(lastName);
+        user.getAddress().setCity(city);
+        user.getAddress().setState(state);
+        user.getAddress().setStreet(street);
+        user.getAddress().setPostCode(postCode);
+        new AsyncTask<User, Void, Void>(){
+            @Override
+            protected Void doInBackground(User... users) {
+                database.getUserDao().updateUser(users[0]);
+                return null;
+            }
+        }.execute(user);
+    }
+
+}
+
