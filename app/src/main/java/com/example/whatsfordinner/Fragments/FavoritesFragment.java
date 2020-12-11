4e2c6334d68cdf4d22ea43640a90dd6e96cd1b91
watsonkalamari <klwatson3@ualr.edu>
2020-12-10 22:32:18 -0600

    Cleaning up code

--- app/src/main/java/com/example/whatsfordinner/Fragments/FavoritesFragment.java
+++ app/src/main/java/com/example/whatsfordinner/Fragments/FavoritesFragment.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.whatsfordinner.R;
 
@@ -18,5 +20,12 @@ public class FavoritesFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.favorite_recipes_display_activity, container, false);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        RecyclerView recyclerView = view.findViewById(R.id.favorite_recipes_recyclerView);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
     }
 }
