86955a55e1da06af9526589e15eb1f655a7a2331
watsonkalamari <klwatson3@ualr.edu>
2020-12-12 14:44:33 -0600

    Work in progress

--- app/src/main/java/com/example/whatsfordinner/adapter/FavoriteListAdapter.java
+++ app/src/main/java/com/example/whatsfordinner/adapter/FavoriteListAdapter.java
@@ -1,17 +1,71 @@
 package com.example.whatsfordinner.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-import com.example.whatsfordinner.entity.Recipe;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.whatsfordinner.R;
+import com.example.whatsfordinner.db.entity.Recipe;
+import com.example.whatsfordinner.viewmodel.DatabaseViewModel;
 
 import java.util.List;
 
-public class FavoriteListAdapter {
+public class FavoriteListAdapter extends RecyclerView.Adapter<FavoriteListAdapter.FavoriteListViewHolder> {
 
+    private final LayoutInflater inflater;
     private List<Recipe> recipes;
-    private Context context;
 
-    public FavoriteListAdapter(Context context, List<Recipe> recipe){
-
+    public FavoriteListAdapter(Context context){
+        inflater = LayoutInflater.from(context);
     }
+
+
+    @Override
+    public FavoriteListViewHolder onCreateViewHolder(ViewGroup parent, int viewType ){
+        View itemView = inflater.inflate(R.layout.favorite_recipes_display_single_line,parent,false);
+        return new FavoriteListViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FavoriteListViewHolder holder, int position){
+        if(recipes!=null) {
+            Recipe current = recipes.get(position);
+            holder.recipetitle.setText(current.getName());
+        }else{
+            holder.recipetitle.setText("NO TITLE");
+
+        }
+    }
+
+    void setRecipes(List<Recipe> Recipe){
+        recipes = Recipe;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount(){
+        if(recipes!=null)
+            return recipes.size();
+        else return 0;
+    }
+
+
+   public class FavoriteListViewHolder extends RecyclerView.ViewHolder{
+        public Bitmap bitmap;
+        public TextView recipetitle;
+
+        private FavoriteListViewHolder (View v){
+            super(v);
+            recipetitle = v.findViewById(R.id.favorite_title_tv);
+        }
+   }
+
+
+
 }
--- app/src/main/java/com/example/whatsfordinner/viewmodel/DatabaseViewModel.java
+++ app/src/main/java/com/example/whatsfordinner/viewmodel/DatabaseViewModel.java
@@ -1,7 +1,35 @@
 package com.example.whatsfordinner.viewmodel;
 
+import android.app.Application;
+
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
 
-public class DatabaseViewModel {
+import com.example.whatsfordinner.db.entity.Recipe;
+import com.example.whatsfordinner.db.entity.User;
+import com.example.whatsfordinner.repository.DataRepository;
 
+import java.util.List;
+
+public class DatabaseViewModel extends AndroidViewModel {
+
+    private DataRepository repository;
+    private LiveData<List<Recipe>> allRecipes;
+    private LiveData<List<User>> allUsers;
+
+    public DatabaseViewModel (Application application){
+        super(application);
+        repository = new DataRepository(application);
+        allRecipes = repository.getAllRecipes();
+        //TODO::add the one for the users
+    }
+
+    public LiveData<List<Recipe>> getAllRecipes() {
+        return allRecipes;
+    }
+    public void insert(Recipe recipe){
+        repository.insert(recipe);
+    }
+    //TODO:: add the getAllUsers()
+    //TODO:: add the insert for the users.
 }
